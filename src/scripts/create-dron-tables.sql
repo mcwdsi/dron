507c4fe7a62409f02c5d32e99a7c1da7b4a9dfb3
William Hogan <hoganwr@gmail.com>
2025-12-22 10:39:42 -0600

    made the datatype field mandatory in disposition and thus disposition.tsv must have it.

--- src/scripts/create-dron-tables.sql
+++ src/scripts/create-dron-tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE rxcui (
 CREATE TABLE disposition (
     curie TEXT PRIMARY KEY,
     label TEXT UNIQUE NOT NULL,
-    datatype TEXT
+    datatype TEXT NOT NULL
 );
 
 CREATE TABLE ingredient (
